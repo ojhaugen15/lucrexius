123e145a514205cfdd2290a93f03e5521cb9fdf9
ojhaugen15 <ojhaugen15@gmail.com>
2025-09-11 11:55:38 -0700

    Create app-history.js
    for those interested in the 'history' of this app (still forthcoming), wrote this up in a couple of minutes to survey the differences between latin and english in terms of the incidence of their shared alphabet...

---
+++ app-history.js
@@ -0,0 +1,13 @@
+function countLetters (inputString) {
+ letterObject = {}
+ searchIndex = 0
+ inputSize = inputString.length
+ inputString = inputString.toUpperCase()
+ while (inputSize > searchIndex) {
+  currentCharacter = inputString[searchIndex]
+  currentCount = letterObject[currentCharacter] || 0
+  letterObject[currentCharacter] = currentCount + 1
+  searchIndex = searchIndex + 1
+ }
+ return letterObject
+}
