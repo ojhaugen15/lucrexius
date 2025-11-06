207855b00a16c7b19a2daf67eb586780ffb4b9ac
ojhaugen15 <ojhaugen15@gmail.com>
2025-11-06 10:40:38 -0800

    Update app-history.js

--- app-history.js
+++ app-history.js
@@ -19,3 +19,26 @@ function countLetters (inputString) {
  }
  return letterObject
 }
+
+`
+wrote this original and very key component in about 10 minutes, a function 
+that surely is useful in many other contexts and applications...
+`
+
+function findText (parentNode) {
+ textContent = parentNode.textContent
+ contentSize = textContent.length
+ searchIndex = 0
+ nodeChildren = parentNode.children
+ numberChildren = nodeChildren.length
+ while (searchIndex < numberChildren) {
+  currentChild = nodeChildren[searchIndex]
+  childContent = currentChild.textContent
+  childSize = childContent.length
+  if ((childSize / contentSize) > .5) {
+   return findText(currentChild)
+  }  
+  searchIndex = searchIndex + 1
+ }
+ return parentNode
+}
